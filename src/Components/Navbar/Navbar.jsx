ae94602f5dc2822512be3e698ddeb0ad273b7cf9
Elham <elhamwaheed777@gmail.com>
2025-01-17 03:08:12 +0500

    Except of Dark Mode

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { IoMdSearch } from "react-icons/io";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Navbar = () => {
   const MenuLinks = [
@@ -26,9 +28,9 @@ const Navbar = () => {
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
       <div className="py-4">
-        <div className="container">
+        <div className="container flex justify-between items-center ">
           {/* Logo and Links Section */}
-          <div>
+          <div className="flex items-center gap-4">
             <a
               className="text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl"
               href="#"
@@ -42,12 +44,32 @@ const Navbar = () => {
             <ul className="flex items-center gap-4">
               {MenuLinks.map((data, index) => (
                 <li key={index}>
-                  <a href={data.link}>{data.name}</a>
+                  <a
+                    href={data.link}
+                    className="inline-block px-4 font-semibold text-gray-500 hover:text-black dark:hover:text-white duration-200"
+                  >
+                    {" "}
+                    {data.name}
+                  </a>
                 </li>
               ))}
             </ul>
           </div>
-          {/* Right Side of Logo */}
+          {/* Navbar Right Section */}
+          <div className="flex justify-between items-center gap-4">
+            {/* Search Bar Section */}
+            <div className="relative group hidden sm:block">
+              <input type="text" placeholder="Search" className="search-bar" />
+              <IoMdSearch className="text-xl text-gray-600 group-hover:text-primary dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-3 duration-200" />
+            </div>
+            {/* Order Button Section */}
+            <button className="relative p-3">
+              <FaShoppingCart className="text-xl text-gray-600 dark:text-gray-400" />
+              <div className="w-4 h-4 bg-red-500 text-white rounded-full absolute top-0 right-0 flex items-center justify-center text-xs ">4</div>
+            </button>
+            {/* Dark Mode Section */}
+            <div></div>
+          </div>
         </div>
       </div>
     </div>
