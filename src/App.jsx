fa0b78ebffc519d755b241b640a98d632ed1a70f
Elham <elhamwaheed777@gmail.com>
2025-01-24 03:59:47 +0500

    Second Bannar Done

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ import Category from "./Components/Category/Category";
 import Category2 from "./Components/Category/Category2";
 import Services from "./Components/Services/Services";
 import Bannar from "./Components/Bannar/Bannar";
-import headphone from "./assets/hero/headphone.png"
+import headphone from "./assets/hero/headphone.png";
 import Products from "./Components/Products/Products";
+import smartwatch2 from "./assets/category/smartwatch2-removebg-preview.png"
 const BannarData = {
   discount: "30% OFF",
   title: "Fine Smile",
@@ -18,6 +19,16 @@ const BannarData = {
     "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
   bgColor: "#f42c37",
 };
+const BannarData2 = {
+  discount:"30% OFF",
+  title:"Happy Hours",
+  date:"14 Jan to 28 Jan",
+  image:smartwatch2,
+  title2:"Smart Solo",
+  title3:"Winter Sale",
+  title4:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+  bgColor:"#2dcc6f",
+};
 const App = () => {
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
@@ -28,6 +39,7 @@ const App = () => {
       <Services />
       <Bannar data={BannarData} />
       <Products />
+      <Bannar data={BannarData2} />
     </div>
   );
 };
