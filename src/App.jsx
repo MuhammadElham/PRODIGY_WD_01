e7184300dc6256c1df7d98782d366e2171d49743
Elham <elhamwaheed777@gmail.com>
2025-01-23 20:51:26 +0500

    Hero Section Done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,19 @@ import Hero from "./Components/Hero/Hero";
 import Category from "./Components/Category/Category";
 import Category2 from "./Components/Category/Category2";
 import Services from "./Components/Services/Services";
+import Bannar from "./Components/Bannar/Bannar";
+import headphone from "./assets/hero/headphone.png"
+const BannarData = {
+  discount: "30% OFF",
+  title: "Fine Smile",
+  date: "10 Jan to 28 Jan",
+  image: headphone,
+  title2: "Air Solo Bass",
+  title3: "Winter Sale",
+  title4:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+  bgColor: "#f42c37",
+};
 const App = () => {
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
@@ -12,6 +25,7 @@ const App = () => {
       <Category />
       <Category2 />
       <Services />
+      <Bannar data={BannarData} />
     </div>
   );
 };
